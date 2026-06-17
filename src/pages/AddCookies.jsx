eb63caecc6e59552d463307e1b3e4870a9680585
Cookie Heaven App <nkatekodebra09@gmail.com>
2026-06-17 22:54:41 +0200

    Initial commit

---
+++ src/pages/AddCookies.jsx
@@ -0,0 +1,12 @@
+
+
+function AddCookies() {
+  return (
+    <div>
+      <h2>Add Cookies</h2>
+      {/* Form to add cookies will go here */}
+    </div>
+  )
+}
+
+export default AddCookies
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Welcome to the Cookie Heaven Dashboard!</p>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Dashboard
